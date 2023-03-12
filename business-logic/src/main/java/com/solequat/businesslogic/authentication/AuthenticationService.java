7033eff0a41550deb1e8eae94186c2bdaef630f2
pro100proger <nazar4075066@gmail.com>
2023-03-12 14:01:40 +0200

    create controller and service for authentication

---
+++ business-logic/src/main/java/com/solequat/businesslogic/authentication/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.solequat.businesslogic.authentication;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.solequat.businesslogic.config.JwtUtil;
+import com.solequat.businesslogic.entity.Role;
+import com.solequat.businesslogic.entity.User;
+import com.solequat.businesslogic.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private  final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+            .firstName(request.getFirstName())
+            .lastName(request.getLastName())
+            .email(request.getEmail())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .role(Role.USER)
+            .build();
+        userRepository.save(user);
+        String jwtToken = jwtUtil.generateToken(user);
+        return AuthenticationResponse
+            .builder()
+            .token(jwtToken)
+            .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+        User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow();
+
+        String jwtToken = jwtUtil.generateToken(user);
+        return AuthenticationResponse
+            .builder()
+            .token(jwtToken)
+            .build();
+    }
+}
